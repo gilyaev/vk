4325ab77d8d03f44744c46b487f8d3d856addab2
Viktor Zhyliaiev <gilyaev@gmail.com>
2016-12-16 09:53:34 +0100

    Added more x :)

--- command.js
+++ command.js
@@ -3,9 +3,9 @@
 var VKT = require('./lib/index.js'),
     vkt = new VKT({
         'mode'  : 'oauth',
-        'appId' : 'xxx',
-        'appSecret' : 'xxx',
-        'token' : 'xxxx'
+        'appId' : 'xxxxxx',
+        'appSecret' : 'xxxxxx,
+        'token' : 'xxxxxxxx'
     });
 
 vkt.command();
