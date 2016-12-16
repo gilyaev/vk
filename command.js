891813781f49483a4f39f98ead882b5b09f7c15e
Viktor Zhyliaiev <zhilyaev-vv@fxclub.org>
2016-12-16 09:42:25 +0100

    Patch update

--- command.js
+++ command.js
@@ -1,10 +1,11 @@
+
 #!/usr/bin/env node
 var VKT = require('./lib/index.js'),
-	vkt = new VKT({
-		'mode'  : 'oauth',
-		'appId' : 'xxx',
-		'appSecret' : 'xxx',
-		'token' : 'xxxx'
-	});
+    vkt = new VKT({
+        'mode'  : 'oauth',
+        'appId' : 'xxx',
+        'appSecret' : 'xxx',
+        'token' : 'xxxx'
+    });
 
 vkt.command();
